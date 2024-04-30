c6eeb7efffd55f7fb000111c372809591db03040
Roman Koriachko <roman_koriachko@epam.com>
2024-04-30 15:21:05 +0300

    add projects section

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Initialization of swipers
+
 const progressCircle = document.querySelector('.autoplay-progress svg');
 const progressContent = document.querySelector('.autoplay-progress span');
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.preview-swiper', {
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
@@ -30,6 +32,25 @@ const swiper = new Swiper('.swiper', {
 	// 	},
 	// },
 });
+
+const projectSwiper = new Swiper('.projects-swiper', {
+	spaceBetween: 30,
+	effect: 'fade',
+	fadeEffect: {
+		crossFade: true,
+	},
+	loop: true,
+	pagination: {
+		el: '.projects-swiper-pagination',
+		clickable: true,
+	},
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
+});
+
+// Services section animation
 
 const servicesItems = document.querySelectorAll('.services-item');
 const serviceItem1 = document.querySelector('.services-item-1');
@@ -65,3 +86,45 @@ serviceItem4.addEventListener('mouseover', () => {
 serviceItem4.addEventListener('mouseout', () => {
 	servicesContainer.classList.remove('hovered-4');
 });
+
+// Change projects-link-wrapper background image when projects-swiper slide change
+
+// const parentElement = document.querySelector('.projects-swiper');
+// const projectsLink = document.querySelector('.projects-link-wrapper');
+
+// function setBackgroundToElementFromImg(img, element) {
+// 	const imgUrl = img.getAttribute('src');
+// 	element.style.backgroundImage = `url(${imgUrl})`;
+// }
+
+// function observeDOMChanges() {
+// 	const parentElement = document.querySelector('.projects-swiper');
+// 	const targetElement = document.querySelector('.projects-link-wrapper');
+
+// 	if (parentElement && targetElement) {
+// 		const observer = new MutationObserver((mutationsList) => {
+// 			for (let mutation of mutationsList) {
+// 				if (
+// 					mutation.type === 'attributes' &&
+// 					mutation.attributeName === 'class'
+// 				) {
+// 					const nextSlide = parentElement.querySelector('.swiper-slide-next');
+// 					if (nextSlide) {
+// 						const img = nextSlide.querySelector('img');
+// 						if (img) {
+// 							setBackgroundToElementFromImg(img, targetElement);
+// 						}
+// 					}
+// 				}
+// 			}
+// 		});
+
+// 		observer.observe(parentElement, {
+// 			attributes: true,
+// 			subtree: true,
+// 			attributeFilter: ['class'],
+// 		});
+// 	}
+// }
+
+// observeDOMChanges();
